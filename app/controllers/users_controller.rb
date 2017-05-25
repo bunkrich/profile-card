62cbd0ad0980eeda1107f8248de5fb646cb80ecf
Brent Unkrich <bunkrich@gmail.com>
2017-05-25 15:43:28 -0400

    added a username for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ get '/login' do
 end
 
 post '/login' do
-  @user = User.find_by(email: params[:user][:email])
+  @user = User.find_by(username: params[:user][:username])
   if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
     redirect "/users/#{@user.id}"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates :email, :google_sheets_code, presence: true
-  validates :email, uniqueness: true
+  validates :email, :google_sheets_code, :username, presence: true
+  validates :email, :username, uniqueness: true
   validates :email, format: { with: /[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+/ }
   # validates :google_sheets_code, format: { with: /spreadsheets\/d\/.+/ }
 
