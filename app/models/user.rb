366b3e3777750cdd74f39d924a73f45989136774
Brent Unkrich <bunkrich@gmail.com>
2017-05-25 12:33:02 -0400

    built model for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+/ }
+
+  has_secure_password
 end
